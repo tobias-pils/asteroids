ca2b957c4b1fac8fa122c268616af4b961e52b54
Tobias Pils <tobias.pils@proton.me>
2025-03-05 15:38:25 +0100

    set framerate and draw player

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
+        screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
